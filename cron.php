52f24522a3590ac2d15973a602f3ee487c8ed4ad
ivancar93 <ivan.stevanovic65@gmail.com>
2017-02-26 18:04:08 +0100

    Updade cron.php file

--- cron.php
+++ cron.php
@@ -3,7 +3,20 @@
 require_once("Twitter.php");
 
 $tw = new Twitter();
-//fill database for 1st time.
-//$tw->fillDataBase();
+$servername1 = "localhost";
+$username1 = "root";
+$password1 = "";
+$dbname1 = "tweetsdb";
+$connections1 = mysqli_connect($servername1,$username1,$password1,$dbname1);
+$query1 = "SELECT title FROM tweets";
+$res1 = mysqli_query($connections1,$query1);
+//check if table tweets is empty
+if(mysqli_num_rows($res1)==0){
+ //fill database's tabel tweets for 1st time if emtpy.
+ $tw->fillDataBase();   
+}
+else{
 //recomended to update databes every 15 min cron job. 
 $tw->cornUpdateDb();
+}
+
